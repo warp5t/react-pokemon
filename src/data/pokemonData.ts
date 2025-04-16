64ac78fea6142129b33d6a3933d734ebb148b8dc
warp5t <dmitrij.didencko@yandex.by>
2025-04-16 12:04:55 +0300

    feat: type pokenom data

---
+++ src/data/pokemonData.ts
@@ -0,0 +1,13 @@
+import { PokemonData } from "../interfaces/Pokemon"
+export const pokemon: Array<PokemonData> = [
+  {"name": "bulbasaur"},
+  {"name": "ivysaur"},
+  {"name": "venusaur"},
+  {"name": "charmander"},
+  {"name": "charmeleon"},
+  {"name": "charizard"},
+  {"name": "squirtle"},
+  {"name": "wartortle"},
+  {"name": "blastoise"},
+  {"name": "caterpie"}
+]
