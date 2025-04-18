f4e43fde12b7ac67b092555982cfe208e6b458e7
warp5t <dmitrij.didencko@yandex.by>
2025-04-18 15:39:48 +0300

    feat: implement type PokeStat and mock data

--- src/data/pokemonData.ts
+++ src/data/pokemonData.ts
@@ -1,4 +1,4 @@
-import { PokemonData } from "../interfaces/Pokemon"
+import { PokemonData,PokeStat } from "../interfaces/Pokemon"
 export const pokemon: Array<PokemonData> = [
   {"name": "bulbasaur"},
   {"name": "ivysaur"},
@@ -11,3 +11,59 @@ export const pokemon: Array<PokemonData> = [
   {"name": "blastoise"},
   {"name": "caterpie"}
 ];
+export const pokeStat: Array<PokeStat> = [
+  {
+    name: "ivysaur",
+    height: 1.0,
+    weight: 13.0,
+    image: "https://img.pokemondb.net/artwork/avif/ivysaur.avif"
+  },
+  {
+    name: "venusaur",
+    height: 2.0,
+    weight: 100.0,
+    image: "https://img.pokemondb.net/artwork/avif/venusaur.avif"
+  },
+  {
+    name: "charmander",
+    height: 0.6,
+    weight: 8.5,
+    image: "https://img.pokemondb.net/artwork/avif/charmander.avif"
+  },
+  {
+    name: "charmeleon",
+    height: 1.1,
+    weight: 19.0,
+    image: "https://img.pokemondb.net/artwork/avif/charmeleon.avif"
+  },
+  {
+    name: "charizard",
+    height: 1.7,
+    weight: 90.5,
+    image: "https://img.pokemondb.net/artwork/avif/charizard.avif"
+  },
+  {
+    name: "squirtle",
+    height: 0.5,
+    weight: 9.0,
+    image: "https://img.pokemondb.net/artwork/avif/squirtle.avif"
+  },
+  {
+    name: "wartortle",
+    height: 1.0,
+    weight: 22.5,
+    image: "https://img.pokemondb.net/artwork/avif/wartortle.avif"
+  },
+  {
+    name: "blastoise",
+    height: 1.6,
+    weight: 85.5,
+    image: "https://img.pokemondb.net/artwork/avif/blastoise.avif"
+  },
+  {
+    name: "caterpie",
+    height: 0.3,
+    weight: 2.9,
+    image: "https://img.pokemondb.net/artwork/avif/caterpie.avif"
+  }
+]
--- src/interfaces/Pokemon.ts
+++ src/interfaces/Pokemon.ts
@@ -5,3 +5,9 @@ export interface PokeCardProps {
   name: string;
   number: string;
 }
+export interface PokeStat {
+  name: string,
+  height: number,
+  weight: number,
+  image: string
+}
