cd26f07c670b6dfd31d72fe0da67b9e19cd1a7e0
warp5t <dmitrij.didencko@yandex.by>
2025-04-26 13:59:21 +0300

    feat: add image NotFoutPage

--- src/screens/notFound/NotFoundPage.tsx
+++ src/screens/notFound/NotFoundPage.tsx
@@ -1,3 +1,13 @@
+import pokemon from "../../assets/icon/giphy.gif"
+
 export const NotFoundPage = () => {
-  return <h1>The page is not found</h1>;
+  return (
+    <>
+    <h1>The page is not found</h1>;
+    <div>
+      <img src={pokemon} alt="pokemon" />
+    </div>
+    </>
+  )
+
 };
