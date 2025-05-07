e372c035d2a1b63e0400cf46a93b4ce28892332f
warp5t <dmitrij.didencko@yandex.by>
2025-05-07 12:56:31 +0300

    feat: implememnt PokeSlicer

---
+++ src/data/pokeSlice/pokeSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit'
+// import { act } from 'react'
+// import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface PokeResults {
+  data: DataPoke,
+  status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+  isLoading: false,
+}
+
+interface InfoPoke {
+  name: string,
+  url: string
+}
+
+interface DataPoke {
+  count: number,
+  next: string,
+  previous: null | string;
+  results: Array<InfoPoke>
+}
+
+const initialState: PokeResults = {
+  data: {
+    count: 1025,
+    next: 'https://pokeapi.co/api/v2/pokemon-species/?offset=20&limit=20',
+    previous: null,
+    results: [
+      {
+        name: 'bulbasaur',
+        url: 'https://pokeapi.co/api/v2/pokemon-species/1/'
+      }
+    ]
+  },
+  status: 'idle',
+  error: null,
+  isLoading: false,
+};
+
+const pokeListSlice = createSlice({
+  name: 'pokeList',
+  initialState,
+  reducers: {
+    setLoading(state, action) {
+      state.isLoading = action.payload
+    },
+    fetchDataPoke(state, action) {
+      state.data.results = action.payload
+    }
+  },
+})
+
+
+export const { setLoading, fetchDataPoke } = pokeListSlice.actions;
+export default pokeListSlice.reducer
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import pokeListReducer from '../data/pokeSlice/pokeSlice'; // Default импорт
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    pokeList: pokeListReducer, // Используем редьюсер
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
