7bdb9064b3e3728f306b261913509dca0e0a1905
warp5t <dmitrij.didencko@yandex.by>
2025-05-18 18:45:07 +0300

    refactor: change name onClick event Pokemons.tsx

--- src/components/Pokemons/Pokemons.tsx
+++ src/components/Pokemons/Pokemons.tsx
@@ -4,11 +4,13 @@ import comparison from '../../assets/icon/arrows.png';
 import { Link } from 'react-router-dom';
 import { capitalizing } from '../../utils/capitalizer';
 import { PokeCardProps } from './pokemonsTyoe';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFavorite, removeFavorite } from '../../slicers/pokeFavorite/pokeFavorite';
 import { AppDispatch, RootState } from '../../store/store';
 import { getComparePokeThunks } from '../../slicers/pokeCompare/compareSlice';
+import { removeComparePokemon } from '../../slicers/pokeCompare/compareSlice';
+
 
 const logging = (name: string) => {
   console.log(name);
@@ -19,11 +21,16 @@ export const PokeCard = ({ name, id }: PokeCardProps) => {
 
   const pokeFavorites = useSelector((state: RootState) => state.pokeFavorites.pokemons);
   const isFavorite = pokeFavorites.some((pokemon) => pokemon.name === name);
-  // const pokeCompareName = useSelector((state: RootState) => state.pokeCompare.data.name);
-  const pokeCompare = useSelector((state: RootState) => state.pokeCompare);
-  // const isCompare = name === pokeCompareName;
+  const pokeCompareData = useSelector((state: RootState) => state.pokeCompare.data);
+  const pokeErrorCompare = useSelector((state: RootState) => state.pokeCompare.error);
   const isCompare = useSelector((state: RootState) =>
     state.pokeCompare.data.some(pokemon => pokemon.name === name));
+
+  useEffect(() => {
+    console.log('link is changed: ', pokeErrorCompare);
+
+  },[pokeErrorCompare])
+
 
   const toggleFavorite = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -39,14 +46,20 @@ export const PokeCard = ({ name, id }: PokeCardProps) => {
 
   const toogleCompare = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    dispatch(getComparePokeThunks({ url: `https://pokeapi.co/api/v2/pokemon/${name}/` }))
+    const isAlreadyCompared = pokeCompareData.some(poke => poke.name === name);
+    if (isAlreadyCompared) {
+      dispatch(removeComparePokemon(Number(id)));
+    } else {
+      dispatch(getComparePokeThunks({ url: `https://pokeapi.co/api/v2/pokemon/${name}/` }));
+    }
   }
 
-  const pokeFavor = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+
+  const infoLOG = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    console.log(isCompare);
-    // console.log('pokeCompareName: ', pokeCompareName);
-    console.log('pokeCompare: ', pokeCompare);
+    console.log('pokeCompareData: ', pokeCompareData);
+    console.log('pokeErrorCompare: ', pokeErrorCompare);
   }
 
   return (
@@ -60,6 +73,7 @@ export const PokeCard = ({ name, id }: PokeCardProps) => {
       <div className={style.pokemons__wrapTitle}>
         <h3>{capitalizing(name)}</h3>
         <div>#{id}</div>
+        <button onClick={infoLOG}>statPoke</button>
       </div>
       <div className={style.pokemons__wrapButton}>
         <button onClick={toggleFavorite} className={isFavorite ? style.pokemons__btn_active : ''}>
@@ -68,7 +82,6 @@ export const PokeCard = ({ name, id }: PokeCardProps) => {
         <button onClick={toogleCompare} className={isCompare ? style.pokemons__btn_active1 : ''}>
           <img src={comparison} alt='comparison' />
         </button>
-        <button onClick={pokeFavor}>statPoke</button>
       </div>
     </Link>
   );
