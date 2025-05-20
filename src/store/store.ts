ba2c97bb66ca0733fdca98c834fa5f73db244d44
warp5t <dmitrij.didencko@yandex.by>
2025-05-20 16:47:30 +0300

    feat: implement pesistanste comparing pokemons

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import pokeListReducer from '../slicers/pokeList/pokeSlice';
 import pokeDetailsReducer from '../slicers/pokeDetails/detailsSlice';
 import favoritesReducer from '../slicers/pokeFavorite/pokeFavorite';
 import compareReducer from '../slicers/pokeCompare/compareSlice';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+// import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 
-const rootReducer = {
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['pokeCompare'],
+  blacklist: ['pokeList','pokeDetails','pokeFavorites'],
+  // stateReconciler: hardSet,
+};
+
+const rootReducer = combineReducers({
   pokeList: pokeListReducer,
   pokeDetails: pokeDetailsReducer,
   pokeFavorites: favoritesReducer,
   pokeCompare: compareReducer,
-};
-
-export const store = configureStore({
-  reducer: rootReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
