9bb4518dba0f4cc1bac2120e7262ee09dba4e05e
warp5t <dmitrij.didencko@yandex.by>
2025-05-21 17:56:35 +0300

    refactor: remove getInitialPageThunks

--- src/slicers/pokeList/pokeSlice.ts
+++ src/slicers/pokeList/pokeSlice.ts
@@ -1,11 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { initialState } from './pokeSlicerType';
 
-export const getInitialPokeThunks = createAsyncThunk('pokeList/fetchInitial', async () => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon-species');
-  return await response.json();
-});
-
 export const getSamePageThunks = createAsyncThunk(
   'pokeList/fetchSamePage',
   async ({ url }: { url: string }) => {
@@ -39,25 +34,6 @@ const pokeListSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(getInitialPokeThunks.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(getInitialPokeThunks.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isInitialLoaded = true;
-        state.data = {
-          count: action.payload.count,
-          next: action.payload.next,
-          previous: action.payload.previous,
-          results: action.payload.results,
-          currentPage: 1,
-        };
-      })
-      .addCase(getInitialPokeThunks.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message || 'Unknown error';
-      })
       // --------------------- pokeThunks ----------------------
       .addCase(getPagePokeThunks.pending, (state) => {
         state.isLoading = true;
