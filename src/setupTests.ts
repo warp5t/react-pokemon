cb1faea058437f85a776bd3c25fc5cdb4fb8e1dd
warp5t <dmitrij.didencko@yandex.by>
2025-05-23 13:37:54 +0300

    fix: fix conflict libraries

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/vitest';
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
+// Очистка после каждого теста
 afterEach(() => {
   cleanup();
 });
