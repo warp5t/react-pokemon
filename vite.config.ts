605b696c0ab8dc06e7458159e059a41866881572
warp5t <dmitrij.didencko@yandex.by>
2025-05-24 21:23:09 +0300

    refactor: remove comments

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom', // Для тестирования React-компонентов
+    environment: 'jsdom',
     coverage: {
       provider: 'istanbul',
       reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx}'], // Анализировать только исходники
+      include: ['src/**/*.{ts,tsx}'],
     },
-    setupFiles: './src/setupTests.ts', // Файл с глобальными настройками
+    setupFiles: './src/setupTests.ts',
   },
 });
