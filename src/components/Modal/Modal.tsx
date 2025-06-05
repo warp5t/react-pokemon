2e30da4489aca98aeb6f3d7e0fc6f21aa814ccd0
warp5t <dmitrij.didencko@yandex.by>
2025-06-05 20:41:26 +0300

    fix: fix apearing modal window #1

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,27 @@
 import ReactDOM from 'react-dom';
 import style from './Modal.module.css';
-
+import { clearCompareError } from '../../slicers/pokeCompare/compareSlice';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
 interface ModalProps {
   toggle: () => void;
 }
 
 export function Modal({ toggle }: ModalProps) {
   const modalRoot = document.getElementById('modal-root') as HTMLDivElement;
+   const dispatch = useDispatch<AppDispatch>();
   return ReactDOM.createPortal(
     <div className={style.modal}>
       <div className={style.modal__wrap}>
         <div className={style.modal__text}>
           You can't chose <br /> more than two pokemons
         </div>
-        <button className={style.modal__btn} onClick={toggle}>
+        <button className={style.modal__btn} onClick={
+          () => {
+            toggle()
+            dispatch(clearCompareError())
+          }
+          }>
           Close
         </button>
       </div>
